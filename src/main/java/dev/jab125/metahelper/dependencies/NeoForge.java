be7e8d7f546741032a57a441b38c56a250fde549
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-02-01 00:21:59 +1100

    add neoforge

--- src/main/java/dev/jab125/metahelper/dependencies/NeoForge.java
+++ src/main/java/dev/jab125/metahelper/dependencies/NeoForge.java
@@ -1,19 +1,24 @@
 package dev.jab125.metahelper.dependencies;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import dev.jab125.metahelper.Main;
+import dev.jab125.metahelper.util.Changelog;
+import dev.jab125.metahelper.util.DiscordWebhook;
 import dev.jab125.metahelper.util.Metadata;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class NeoForge implements Deps {
     public static final String FORGE_URL = "https://maven.neoforged.net/net/neoforged/neoforge/maven-metadata.xml";
-
+    private final List<JsonObject> changelogs = new ArrayList<>();
     @Override
     public JsonObject get(List<String> mcVersions, JsonObject previous) throws Throwable {
         JsonObject obj = new JsonObject();
@@ -26,13 +31,28 @@ public class NeoForge implements Deps {
             Metadata mavenMetadata = xmlMapper.readValue(response.body().bytes(), Metadata.class);
             List<String> versions = reverse(mavenMetadata.versioning.versions.stream()).toList();
             for (String mcVersion : mcVersions) {
+                JsonElement neoforge1 = previous.get("neoforge").getAsJsonObject().get(mcVersion);
+                String prevNeoForgeVer;
+                if (neoforge1 == null || neoforge1.isJsonNull()) {
+                    prevNeoForgeVer = null;
+                } else {
+                    prevNeoForgeVer = neoforge1.getAsString();
+                }
                 try {
-                    neoforge.addProperty(mcVersion, "net.neoforged:neoforge:" + versions.stream().filter(a -> {
+                    String neoforgeVer = "net.neoforged:neoforge:" + versions.stream().filter(a -> {
                         String[] split = a.split("\\.");
                         String major = split[0];
                         String minor = split[1];
                         return mcVersion.equals("1." + major + "." + minor);
-                    }).findFirst().orElseThrow());
+                    }).findFirst().orElseThrow();
+                    if (!neoforgeVer.equals(prevNeoForgeVer)) {
+                        JsonObject object = new JsonObject();
+                        object.addProperty("minecraft", mcVersion);
+                        object.addProperty("previous", prevNeoForgeVer == null ? "" : prevNeoForgeVer.substring("net.neoforged:neoforge:".length()));
+                        object.addProperty("current", neoforgeVer.substring("net.neoforged:neoforge:".length()));
+                        changelogs.add(object);
+                    }
+                    neoforge.addProperty(mcVersion, neoforgeVer);
                 } catch (Throwable t) {
                     System.err.println("Failed to fetch version for " + mcVersion);
                 }
@@ -40,6 +60,23 @@ public class NeoForge implements Deps {
             obj.add("neoforge", neoforge);
         }
         return obj;
+    }
+
+    @Override
+    public List<Changelog> changelogs(JsonObject before, JsonObject after) {
+        ArrayList<DiscordWebhook.EmbedObject> embedObjects = new ArrayList<>();
+        for (JsonObject changelog : this.changelogs) {
+            String minecraft = changelog.getAsJsonPrimitive("minecraft").getAsString();
+            String previous = changelog.getAsJsonPrimitive("previous").getAsString();
+            String current = changelog.getAsJsonPrimitive("current").getAsString();
+            DiscordWebhook.EmbedObject embedObject = new DiscordWebhook.EmbedObject().setColor(Color.ORANGE);
+            embedObject.setTitle("New NeoForge version!");
+            embedObject.addField("Minecraft version", minecraft, true);
+            embedObject.addField("Latest", "**" + previous + "** -> **" + current + "**", true);
+            embedObjects.add(embedObject);
+        }
+        if (embedObjects.isEmpty()) return Deps.super.changelogs(before, after);
+        return List.of(new Changelog(null, embedObjects));
     }
 
     @Override
